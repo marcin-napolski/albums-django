acf8557e5bef9cbba03e111e1dd04e7774267032
Marcin Napolski <marcin.napolski@gmail.com>
2020-06-19 13:59:25 +0200

    Album model + definition of Postgre

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    image = models.ImageField(upload_to='images/')
+    artist = models.CharField(max_length=20)
+    title = models.CharField(max_length=30)
+    summary = models.CharField(max_length=200)
